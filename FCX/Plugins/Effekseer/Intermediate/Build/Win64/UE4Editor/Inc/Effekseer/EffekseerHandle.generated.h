556df0bfe56ab0b34b376adb5313688a1fb2a062
yuta miyake <myuta.shusyoku@gmail.com>
2019-10-21 12:39:04 +0900

    save work.

--- FCX/Plugins/Effekseer/Intermediate/Build/Win64/UE4Editor/Inc/Effekseer/EffekseerHandle.generated.h
+++ FCX/Plugins/Effekseer/Intermediate/Build/Win64/UE4Editor/Inc/Effekseer/EffekseerHandle.generated.h
@@ -13,7 +13,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define EFFEKSEER_EffekseerHandle_generated_h
 
-#define FCreationExhibition_Plugins_Effekseer_Source_Effekseer_Public_EffekseerHandle_h_14_GENERATED_BODY \
+#define FCX_Plugins_Effekseer_Source_Effekseer_Public_EffekseerHandle_h_14_GENERATED_BODY \
 	friend struct Z_Construct_UScriptStruct_FEffekseerHandle_Statics; \
 	static class UScriptStruct* StaticStruct();
 
@@ -21,7 +21,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 template<> EFFEKSEER_API UScriptStruct* StaticStruct<struct FEffekseerHandle>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FCreationExhibition_Plugins_Effekseer_Source_Effekseer_Public_EffekseerHandle_h
+#define CURRENT_FILE_ID FCX_Plugins_Effekseer_Source_Effekseer_Public_EffekseerHandle_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
